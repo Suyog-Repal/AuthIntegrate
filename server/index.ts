823135b990eae46ad92663d535424f907955d498
Suyog Repal <suyog.repal23@spit.ac.in>
2026-04-09 00:25:35 +0530

    updated server/db.ts

--- server/index.ts
+++ server/index.ts
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
     const __dirname = path.dirname(__filename);
     const clientPath = process.env.DIST_DIR
       ? path.resolve(process.env.DIST_DIR)
-      : path.resolve(__dirname, "public");
+      : path.resolve(process.cwd(), "dist", "public");
 
     app.use(express.static(clientPath));
     app.use("*", (_req, res) => {
